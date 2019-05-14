62593c414b6440b65b4350fd358f0528ebabb2f0
kingold5 <xingran_wang@icloud.com>
2019-05-14 19:13:15 +0200

    change errors to log10

--- compare.py
+++ compare.py
@@ -4,15 +4,20 @@ import settings
 
 settings.init()
 
-GPU_TIME = np.loadtxt(settings.Dir_PERFORMANCE+"/GPU_time.txt")
-GPU_ERRORS = np.loadtxt(settings.Dir_PERFORMANCE+"/GPU_errors.txt")
-CPU_TIME = np.loadtxt(settings.Dir_PERFORMANCE+"/CPU_time.txt")
-CPU_ERRORS = np.loadtxt(settings.Dir_PERFORMANCE+"/CPU_errors.txt")
+gpu_time = np.loadtxt(settings.Dir_PERFORMANCE+"/GPU_time.txt")
+gpu_errors = np.loadtxt(settings.Dir_PERFORMANCE+"/GPU_errors.txt")
+cpu_time = np.loadtxt(settings.Dir_PERFORMANCE+"/CPU_time.txt")
+cpu_errors = np.loadtxt(settings.Dir_PERFORMANCE+"/CPU_errors.txt")
+
+gpu_errors = np.log10(gpu_errors)
+cpu_errors = np.log10(cpu_errors)
 
 fig, ax = plt.subplots()
-ax.plot(CPU_TIME, CPU_ERRORS, label='CPU', color='blue')
-ax.plot(GPU_TIME, GPU_ERRORS, label='GPU', color='red')
+ax.plot(cpu_time, cpu_errors, label='CPU', color='blue')
+ax.plot(gpu_time, gpu_errors, label='GPU', color='red')
 legend = ax.legend(loc='upper right', fontsize='x-large')
 plt.xlabel('time/s')
-plt.ylabel('errors')
+plt.ylabel('errors/log10')
 plt.show()
+
+print(cpu_errors)
