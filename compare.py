c74ade6b6230fddb4e1f720ccda0345a19659c44
kingold5 <xingran_wang@icloud.com>
2019-05-14 13:07:37 +0200

    typo error

--- compare.py
+++ compare.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import settings
 
+settings.init()
+
 GPU_TIME = np.loadtxt(settings.HOME+"/Documents/python/GPU_time.txt")
 GPU_ERRORS = np.loadtxt(settings.HOME+"/Documents/python/GPU_errors.txt")
 CPU_TIME = np.loadtxt(settings.HOME+"/Documents/python/CPU_time.txt")
@@ -10,6 +12,6 @@ CPU_ERRORS = np.loadtxt(settings.HOME+"/Documents/python/CPU_errors.txt")
 fig, ax = plt.subplots()
 ax.plot(CPU_TIME, CPU_ERRORS, label='CPU', color='blue')
 ax.plot(GPU_TIME, GPU_ERRORS, label='GPU', color='red')
-legend = ax.legend(loc='top right', fontsize='x-large')
+legend = ax.legend(loc='upper right', fontsize='x-large')
 plt.xlabel('time/s')
 plt.ylabel('errors')
