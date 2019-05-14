8e0b1ce8a3c9108d1bef3a7aef4edebc6e9aad78
kingold5 <xingran_wang@icloud.com>
2019-05-14 12:28:52 +0200

    add plot

---
+++ compare.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import settings
+
+GPU_TIME = np.loadtxt(settings.HOME+"/Documents/python/GPU_time.txt")
+GPU_ERRORS = np.loadtxt(settings.HOME+"/Documents/python/GPU_errors.txt")
+CPU_TIME = np.loadtxt(settings.HOME+"/Documents/python/CPU_time.txt")
+CPU_ERRORS = np.loadtxt(settings.HOME+"/Documents/python/CPU_errors.txt")
+
+fig, ax = plt.subplots()
+ax.plot(CPU_TIME, CPU_ERRORS, label='CPU', color='blue')
+ax.plot(GPU_TIME, GPU_ERRORS, label='GPU', color='red')
+legend = ax.legend(loc='top right', fontsize='x-large')
+plt.xlabel('time/s')
+plt.ylabel('errors')
