dd65c56e70f34cee6dd0849452caac2599672ffc
kingold5 <xingran_wang@icloud.com>
2019-05-15 13:27:32 +0200

    modilfied part of gpu_calculation

--- gpu_calculation.py
+++ gpu_calculation.py
@@ -11,7 +11,6 @@ kernel_code_template = """
 #define MAT_HEIGHT %(MAT_HEIGHT)s
 #define T_HEIGHT %(T_HEIGHT)s
 
-//TODO USE SHARED MEMORY TO CACHE VECTOR
 __global__ void mul_mat_t_vec(double *result, double *mat, double *vec,
 unsigned int index_m){
     const unsigned int row = blockIdx.x*blockDim.x + threadIdx.x;
@@ -127,10 +126,14 @@ unsigned int MAT_WIDTH_ALL){
 }
 """
 
+# TODO 
+# if matrix A cannot be even blocked,
+# append ZEROs to make MAT_WIDTH_ALL % Block = 0
 
 class GPU_Calculation:
     T_WIDTH_TRANS = 64
     T_WIDTH = 128
+    T_HEIGHT = 1024
 
     def __init__(self, A, Block):
         self.Block = Block
@@ -142,12 +145,21 @@ class GPU_Calculation:
                 'MAT_WIDTH': self.MAT_WIDTH,
                 'MAT_HEIGHT': self.MAT_HEIGHT,
                 'T_WIDTH_TRANS': self.T_WIDTH_TRANS,
-                'T_WIDTH': self.T_WIDTH
+                'T_WIDTH': self.T_WIDTH,
+                'T_HEIGHT': self.T_HEIGHT
                 }
         mod = SourceModule(kernel_code)
         self.mul_mat_t_vec = mod.get_function("mul_mat_t_vec")
+        self.mul_mat_t_vec_diffsize = mod.get_function(
+                "mul_mat_t_vec_diffsize")
         self.mul_mat_vec = mod.get_function("mul_mat_vec")
         self.get_diag_ATA = mod.get_function("get_diag_ATA")
+
+        # calculate grid dimension for mul_mat_t_vec_diffsize
+        self.blockCols = np.int(
+                (MAT_WIDTH+T_HEIGHT-1)/T_HEIGHT)
+        self.blockRows = np.int(
+                (MAT_HEIGHT+T_WIDTH_TRANS-1)/T_WIDTH_TRANS)
 
     def init_cpu_array(self, A):
         self.A_b = np.hsplit(A, self.Block)
@@ -159,7 +171,14 @@ class GPU_Calculation:
             (self.MAT_WIDTH+self.T_WIDTH-1) // self.T_WIDTH
         self.result_t = np.empty(
                 (self.MAT_WIDTH, self.SPLIT_TRANS), np.float64)
+
+        # cpu result when using shared memory and reduce algorithm
         self.result_t_shmem = np.empty(
+                (self.MAT_WIDTH, 1), np.float64)
+
+        # cpu result for mul_mat_t_vec_diffsize
+        # all set to zero
+        self.result_t_diffsize = np.zeros(
                 (self.MAT_WIDTH, 1), np.float64)
         self.result = np.empty(
                 (self.MAT_HEIGHT, self.SPLIT), np.float64)
@@ -173,6 +192,10 @@ class GPU_Calculation:
                 float64_size*self.MAT_WIDTH)
         self.result_t_gpu = gpuarray.to_gpu(self.result_t)
         self.result_t_shmem_gpu = gpuarray.to_gpu(self.result_t_shmem)
+
+        # gpu array for result_t_diffsize
+        self.result_t_diffsize_gpu = gpuarray.to_gpu(
+                self.result_t_diffsize)
         self.result_gpu = gpuarray.to_gpu(self.result)
 
     @property
@@ -218,3 +241,13 @@ class GPU_Calculation:
                 self.result, axis=1, dtype=np.float64)[:, np.newaxis]
 
         return result
+
+    # matrix.T@vector for different size matrix
+    def mat_tmulvec_diffsize(self, index_m, s11):
+        index_m = np.uint32(index_m)
+        cuda.memcpy_htod(self.s11_gpu, s11)
+        self.mul_mat_t_vec_diffsize(
+                self.result_t_diffsize_gpu, self.A_b_gpu, self.s11_gpu,
+                index_m,
+                block=(self.T_HEIGHT, 1, 1),
+                grid=()
