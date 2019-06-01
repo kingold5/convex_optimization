af5c2f668d23ee5eade5c1cba82ee224bc4c68ec
kingold5 <xingran_wang@icloud.com>
2019-06-01 18:10:17 +0200

    modify as modulation

--- lasso.py
+++ lasso.py
@@ -9,50 +9,28 @@ import time
 import numpy as np
 from cpu_calculation import element_proj, soft_thresholding, error_crit,\
                           fun_b_k
-from gpu_calculation import GPU_Calculation
-from parameters import parameters
 import settings
 
 settings.init()
-# load parameters from file
-READ_FLAG = True
-# write parameters to file
-SAVE_FLAG = False
-# number of blocks
-BLOCK = 1
-# col of matrix A
-K = 1024
-# row of matrix A
-N = 1024
-# density of sparse vector
-DENSITY = 0.4
-# error bound
-ERR_BOUND = 1e-4
-# maximum number of iterations
-ITER_MAX = 1000*BLOCK
 
-(A, x_true, b, mu) = parameters(N, K, DENSITY, SAVE_FLAG, READ_FLAG)
 
-#################################################
-# ###divide A, x, diagonal ATA blockwise#####
-#################################################
-gpu_cal = GPU_Calculation(A, BLOCK)
-x = np.zeros((A.shape[1], 1))
-x_block = np.vsplit(x, BLOCK)
-x_block = np.asarray(x_block)
-d_ATA = gpu_cal.diag_ATA
+def lasso(gpu_cal, d_ATA, A, b, mu, BLOCK, ERR_BOUND, ITER_MAX):
+    # initialize
+    x = np.zeros((A.shape[1], 1))
+    x_block = np.vsplit(x, BLOCK)
+    x_block = np.asarray(x_block)
+    Ax = np.array([np.dot(gpu_cal.A_b[k], x_block[k]) for k in range(BLOCK)])
+    block_Cnt = 0
 
-# initialize Ax
-Ax = np.array([np.dot(gpu_cal.A_b[k], x_block[k]) for k in range(BLOCK)])
-block_Cnt = 0
-time_cnt = []
-errors = []
-time_mul = 0
-time_mul_t = 0
+    # set time and error counter
+    time_cnt = []
+    errors = []
+    # time_mul = 0
+    # time_mul_t = 0
 
-if __name__ == '__main__':
     start = time.time()
     time_cnt.append(start-start)
+
     for t in range(ITER_MAX):
         # select mth block
         m = t % BLOCK
@@ -60,9 +38,9 @@ if __name__ == '__main__':
         result_s11 = Ax[m] - b_k
 
         # result_s13 = gpu_cal.mat_tmulvec(m, result_s11)
-        time_s = time.time()
+        # time_s = time.time()
         result_s13 = gpu_cal.mat_tMulVec_DiffSize(m, result_s11)
-        time_mul_t += time.time() - time_s
+        # time_mul_t += time.time() - time_s
 
         # s14
         rx = np.multiply(d_ATA[m], x_block[m]) - result_s13
@@ -72,11 +50,11 @@ if __name__ == '__main__':
         # result_s21 = Bx_p - x_p
         descent_D = Bx-x_block[m]
 
-        time_s = time.time()
+        # time_s = time.time()
         # result_s23 = gpu_cal.matmulvec(m, descent_D)
         # result_s23 = gpu_cal.matMulVec_DiffSize(m, descent_D)
         result_s23 = gpu_cal.matMulVec_DST(m, descent_D)
-        time_mul += time.time() - time_s
+        # time_mul += time.time() - time_s
 
         # stepsize
         r_1 = np.dot(
@@ -110,16 +88,41 @@ if __name__ == '__main__':
 
         # x(t+1) = x(t)+r(Bx(t)-x(t))
         x_block[m] += r*(Bx-x_block[m])
-        x = np.vstack(x_block)
+        # x = np.vstack(x_block)
         # Ax(t+1)
         Ax[m] += r*result_s23
         time_cnt.append(time.time()-start)
+
     print("Time used: ", time_cnt[-1], "s.",
           "With", t+1, "loops, and ",
           BLOCK, "blocks.")
+    # print("matrix@vector:", time_mul, "s, matrix.T@vector:", time_mul_t)
 
-    print("matrix@vector:", time_mul, "s, matrix.T@vector:", time_mul_t)
-    PERFORMANCE = False
-    if PERFORMANCE:
-        np.savetxt(settings.Dir_PERFORMANCE+"/GPU_time.txt", time_cnt)
-        np.savetxt(settings.Dir_PERFORMANCE+"/GPU_errors.txt", errors)
+    # PERFORMANCE = False
+    # if PERFORMANCE:
+    #     np.savetxt(settings.Dir_PERFORMANCE+"/GPU_time.txt", time_cnt)
+    #     np.savetxt(settings.Dir_PERFORMANCE+"/GPU_errors.txt", errors)
+
+    return time_cnt[-1]
+
+
+# # load parameters from file
+# READ_FLAG = True
+# # write parameters to file
+# SAVE_FLAG = False
+# # number of blocks
+# BLOCK = 1
+# # col of matrix A
+# K = 1024
+# # row of matrix A
+# N = 1024
+# # density of sparse vector
+# DENSITY = 0.4
+# # error bound
+# ERR_BOUND = 1e-4
+# # maximum number of iterations
+# ITER_MAX = 1000*BLOCK
+# (A, x_true, b, mu) = parameters(N, K, DENSITY, SAVE_FLAG, READ_FLAG)
+# gpu_cal = GPU_Calculation(A, BLOCK)
+# d_ATA = gpu_cal.diag_ATA
+# lasso(gpu_cal, d_ATA, A, b, mu, BLOCK, ERR_BOUND, ITER_MAX)
