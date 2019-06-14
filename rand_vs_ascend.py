c4c33b9cc228e1568e970f17df2fb5187124feab
kingold5 <xingran_wang@icloud.com>
2019-06-14 15:39:58 +0200

    add different T_width iteration option

--- rand_vs_ascend.py
+++ rand_vs_ascend.py
@@ -37,74 +37,75 @@ ERR_BOUND = 1e-4
 # generating matrix and not save
 READ_FLAG = False
 SAVE_FLAG = False
-INSTANCE = 50
+INSTANCE = 1
 
 
-for n_exp in np.arange(7, 8):
+for n_exp in np.arange(9, 10):
     N = 2 ** n_exp
     # was 1-16(0, 4)
     for k_plus in np.arange(3, 4):
         k_exp = n_exp + k_plus
         K = 2 ** k_exp
         # was np.arange(1+k_plus)
-        for b_exp in np.arange(3, 4):
+        for b_exp in np.arange(1, 2):
             BLOCK = 2 ** b_exp
-            # lasso with ascend index
-            time_comp = []
-            err_lasso = []
-            time_lasso = []
-
-            # lasso with random index
-            time_comp_r = []
-            err_lasso_r = []
-            time_lasso_r = []
-
-            time_winit = 0
-            ITER_MAX = 2000
-
-            for ii in range(INSTANCE):
-                # make sure every random seed is unique
-                time.sleep(1)
-                (A, x_true, b, mu) = parameters(
-                    N, K, DENSITY,
-                    SAVE_FLAG, READ_FLAG, SILENCE=False)
-
-                t_init = time.time()
+            for t_width in np.arange(4, 5):
                 # set thread width
-                GPU_Calculation.T_WIDTH_TRANS = 16
-                GPU_Calculation.T_WIDTH = 16
-                gpu_cal = GPU_Calculation(A, BLOCK)
-                d_ATA = gpu_cal.diag_ATA
-                time_winit += time.time() - t_init
+                GPU_Calculation.T_WIDTH_TRANS = 2 ** t_width
+                GPU_Calculation.T_WIDTH = 2 ** t_width
+                # lasso with ascend index
+                time_comp = []
+                err_lasso = []
+                time_lasso = []
 
-                # let gpu warmup the first round
-                lasso(gpu_cal, d_ATA, A, b, mu, BLOCK,
-                      ITER_MAX, ERR_BOUND, SILENCE=False)
-                time_comp.append(
+                # lasso with random index
+                time_comp_r = []
+                err_lasso_r = []
+                time_lasso_r = []
+
+                time_winit = 0
+                ITER_MAX = 2000
+
+                for ii in range(INSTANCE):
+                    # make sure every random seed is unique
+                    time.sleep(1)
+                    (A, x_true, b, mu) = parameters(
+                        N, K, DENSITY,
+                        SAVE_FLAG, READ_FLAG, SILENCE=True)
+
+                    t_init = time.time()
+                    gpu_cal = GPU_Calculation(A, BLOCK)
+                    d_ATA = gpu_cal.diag_ATA
+                    time_winit += time.time() - t_init
+
+                    # let gpu warmup the first round
                     lasso(gpu_cal, d_ATA, A, b, mu, BLOCK,
-                          ITER_MAX, ERR_BOUND,
-                          err_iter=err_lasso, time_iter=time_lasso,
-                          SILENCE=False, DEBUG=False))
+                          ITER_MAX, ERR_BOUND, SILENCE=True)
+                    time_comp.append(
+                        lasso(gpu_cal, d_ATA, A, b, mu, BLOCK,
+                              ITER_MAX, ERR_BOUND,
+                              err_iter=err_lasso, time_iter=time_lasso,
+                              SILENCE=True, DEBUG=False))
 
-                time_comp_r.append(
-                    lasso_rand(gpu_cal, d_ATA, A, b, mu, BLOCK,
-                               ERR_BOUND, ITER_MAX,
-                               err_lasso_r, time_lasso_r,
-                               SILENCE=False, DEBUG=False))
+                    # time_comp_r.append(
+                    #     lasso_rand(gpu_cal, d_ATA, A, b, mu, BLOCK,
+                    #                ITER_MAX, ERR_BOUND,
+                    #                err_lasso_r, time_lasso_r,
+                    #                SILENCE=True, DEBUG=False))
 
-            time_comp = np.asarray(time_comp)
-            time_comp_r = np.asarray(time_comp_r)
+                time_comp = np.asarray(time_comp)
+                # time_comp_r = np.asarray(time_comp_r)
 
-            print('matrix size ({:-5}, {:-5})'.format(N, K)
-                  + ', block size: ' + str(BLOCK)
-                  + ', T_WIDTH=T_WIDTH_TRANS length: '
-                  + str(GPU_Calculation.T_WIDTH)
-                  + ', Ascend index computation time:'
-                  + f'{np.mean(time_comp):.6f}' + ' s'
-                  + ', standard deviation: '
-                  + f'{np.std(time_comp):.6f}')
-            print('Random index lasso computation time:',
-                  np.mean(time_comp_r), 's.',
-                  'standard deviation', np.std(time_comp_r))
-            # print(time_winit / INSTANCE, 's.')
-            plt_fig(err_lasso, time_lasso, err_lasso_r, time_lasso_r)
+                print('matrix size ({:-5}, {:-5})'.format(N, K)
+                      + ', block size: ' + str(BLOCK)
+                      + ', T_WIDTH=T_WIDTH_TRANS length: '
+                      + str(GPU_Calculation.T_WIDTH)
+                      + ', Ascend index computation time:'
+                      + f'{np.mean(time_comp):.6f}' + ' s'
+                      + ', standard deviation: '
+                      + f'{np.std(time_comp):.6f}')
+                # print('Random index lasso computation time:',
+                #       np.mean(time_comp_r), 's.',
+                #       'standard deviation', np.std(time_comp_r))
+                # print(time_winit / INSTANCE, 's.')
+                # plt_fig(err_lasso, time_lasso, err_lasso_r, time_lasso_r)
