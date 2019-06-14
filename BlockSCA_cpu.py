ebe504eb3a21da67e2efc3275af8d3e4bac96490
kingold5 <xingran_wang@icloud.com>
2019-06-14 15:38:57 +0200

    rewrite A_block_p initialization, time_cnt & error as np.ndarray, print

--- BlockSCA_cpu.py
+++ BlockSCA_cpu.py
@@ -16,17 +16,17 @@ import settings
 
 settings.init()
 # load parameters from file
-READ_FLAG = True
+READ_FLAG = False
 # write parameters to file
 SAVE_FLAG = False
 # number of processors is 4=3+1
-P = 8
+P = 4
 # number of blocks
-BLOCK = 1
+BLOCK = 2
 # col of matrix A
-K = 512
+K = 2048
 # row of matrix A
-N = 512
+N = 256
 # density of sparse vector
 DENSITY = 0.4
 # error bound
@@ -41,38 +41,29 @@ ITER_MAX = 1000*BLOCK
 #################################################
 count = 0
 max_Count = BLOCK*P
-p_Len = len(A[0])//max_Count
-A_block_p = [[] for i in range(BLOCK)]
-A_block = []
-x_block = []
-for k in range(BLOCK):
-    for j in range(P):
-        # divide each block into P pieces for parallel algorithm
-        if count < max_Count - 1:
-            A_block_p[k].append(A[:, count*p_Len: (count+1)*p_Len])
-            count += 1
-        elif count == max_Count - 1:
-            A_block_p[k].append(A[:, count*p_Len: len(A[0])])
-            count += 1
-        else:
-            print("A_block_p failed to create blocks!")
-    # stack each block
-    A_block.append(np.hstack(A_block_p[k]))
-    x_block.append(np.zeros((len(A_block[-1][0]), 1)))
+p_len = A.shape[1]//max_Count
+p_hei = A.shape[0]
+
+if (BLOCK * P == 1):
+    A_block_p = A
+else:
+    A_block_p = A.reshape(
+        2, -1, p_hei, p_len).transpose((1, 2, 0, 3)).reshape(
+            BLOCK, P, p_hei, p_len)
+
+x_block = np.zeros((BLOCK, np.int(A.shape[1]/BLOCK), 1))
 x = np.vstack(x_block)
-d_ATA = fun_diag_ATA(A_block)
-A_block = np.asarray(A_block)
-A_block_p = np.asarray(A_block_p)
+d_ATA = fun_diag_ATA(A_block_p)
 
 # initialize Ax
-Ax = np.array([A_block[k]@x_block[k] for k in range(BLOCK)])
+Ax = np.zeros((BLOCK, A.shape[0], 1))
 block_Cnt = 0
-errors = []
-time_cnt = []
+errors = np.zeros((ITER_MAX))
+time_cnt = np.zeros((ITER_MAX))
 
 if __name__ == '__main__':
     start = time.time()
-    time_cnt.append(start-start)
+    time_cnt[0] = 0
     pool = Pool(processes=P)
     for t in range(ITER_MAX):
         # select mth block
@@ -90,8 +81,8 @@ if __name__ == '__main__':
         # s15
         Bx = np.multiply(np.divide(1.0, d_ATA[m]), soft_t)
         # result_s21 = Bx_p - x_p
-        descent_D = Bx-x_block[m]
-        result_s21 = fun_dd_p(A_block_p[m], descent_D)
+        descent_D = Bx - x_block[m]
+        result_s21 = fun_dd_p(P, descent_D)
         # result_s22 = A_P(Bx_P - X_P)
         result_s22 = pool.starmap(fun_s22, zip(A_block_p[m], result_s21))
         # result_s23 = A(Bx-x)
@@ -105,16 +96,17 @@ if __name__ == '__main__':
         else:
             r = element_proj(-r_1/r_2, 0, 1)
 
-        errors.append(error_crit(result_s13, x_block[m], mu))
-        # opti_value = 0.5*(result_s11.T@result_s11) + mu*np.sum(np.abs(x))
-        # opti_value2 = 0.5*np.sum(np.power(A@x-b)) + mu*np.sum(np.abs(x))
+        errors[t] = error_crit(result_s13, x_block[m], mu)
+        # # opti_value = 0.5*np.sum(np.power(result_s11, 2)) +\
+        # #              mu*np.sum(np.abs(x_block[m]))
+        # opti_value2 = 0.5*np.sum(np.power(A@x-b, 2)) + mu*np.sum(np.abs(x))
         # print("Loop ", t,
         #       " block ", m,
-        #       " updated, with Error ", errors[-1],
-        #       " Optimum Value %f " % opti_value,
+        #       " updated, with Error ", errors[t],
+        #       " Optimum Value %f " % opti_value2,
         #       " Stepsize r = %f" % r)
 
-        if errors[-1] < ERR_BOUND:
+        if errors[t] < ERR_BOUND:
             block_Cnt += 1
         if BLOCK - 1 == m:
             if block_Cnt == BLOCK:
@@ -124,12 +116,14 @@ if __name__ == '__main__':
 
         # x(t+1) = x(t)+r(Bx(t)-x(t))
         x_block[m] += r*(Bx-x_block[m])
-        x = np.vstack(x_block)
+        # x = np.vstack(x_block)
         # Ax(t+1)
         Ax[m] += r*result_s23
-        time_cnt.append(time.time()-start)
-    print("Time used: ", time_cnt[-1], "s.",
-          "With ", t+1, "loops.")
+        time_cnt[t+1] = time.time()-start
+    print('block number: ' + str(BLOCK)
+          + ', cores number: ' + str(P)
+          + ', time used: ' + str(time_cnt[t]) + 's, '
+          + 'with ' + str(t+1) + ' loops.')
     pool.close()
     pool.join()
 
