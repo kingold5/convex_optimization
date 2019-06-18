6d4fb0ef0898ebbb0868d65bb8f278b3b1ce6b34
kingold5 <xingran_wang@icloud.com>
2019-06-18 11:52:00 +0200

    change ClassLassoCPU as parent Class

--- lasso.py
+++ lasso.py
@@ -17,15 +17,16 @@ import settings
 settings.init()
 
 
-class ClassLasso:
-    def __init__(self, gpu_cal, d_ATA, A, b, mu, BLOCK, ITER_MAX):
-        self.gpu_cal = gpu_cal
+class ClassLassoCPU:
+    def __init__(self, A_block_p, d_ATA, A, b, mu, BLOCK, P, ITER_MAX):
+        self.A_block_p = A_block_p
         self.d_ATA = d_ATA
         self.A = A
         self.A_SHAPE = A.shape
         self.b = b
         self.mu = mu
         self.BLOCK = BLOCK
+        self.P = P
         self.ITER_MAX = ITER_MAX
 
     def index_get(self, t):
@@ -51,141 +52,6 @@ class ClassLasso:
     def time_record(self, time_iter, t, start):
         if self.TIME_RCD:
             time_iter[t+1] = time.time() - start
-
-    def rlt_display(self, SILENCE, t_elapsed, t):
-        if not SILENCE:
-            print('GPU Ascend index, time used: {:.8f} s, '
-                  'with {:-4} loops, and block number: {:-2}.'.format(
-                      t_elapsed, t+1, self.BLOCK))
-
-    def run(self, ERR_BOUND=None, err_iter=None, time_iter=None,
-            SILENCE=False, DEBUG=False):
-        # initialize
-        self.DEBUG = DEBUG
-        if isinstance(ERR_BOUND, float):
-            IS_BOUNDED = True
-        else:
-            IS_BOUNDED = False
-
-        if isinstance(err_iter, np.ndarray):
-            self.ERR_RCD = True
-        else:
-            self.ERR_RCD = False
-
-        if isinstance(time_iter, np.ndarray):
-            self.TIME_RCD = True
-        else:
-            self.TIME_RCD = False
-
-        x = np.zeros((self.A_SHAPE[1], 1))
-        x_block = np.asarray(np.vsplit(x, self.BLOCK))
-        Ax = np.zeros((self.BLOCK, self.A_SHAPE[0], 1))
-
-        block_Cnt = 0
-
-        # time_mul = 0
-        # time_mul_t = 0
-
-        start = time.time()
-        if self.TIME_RCD:
-            time_iter[0] = 0
-
-        for t in range(self.ITER_MAX):
-            # select mth block
-            m = self.index_get(t)
-            b_k = fun_b_k(Ax, self.b, m)
-            result_s11 = Ax[m] - b_k
-
-            # result_s13 = gpu_cal.mat_tmulvec(m, result_s11)
-            # time_s = time.time()
-            result_s13 = self.gpu_cal.mat_tMulVec_DiffSize(m, result_s11)
-            # time_mul_t += time.time() - time_s
-
-            # s14
-            rx = np.multiply(self.d_ATA[m], x_block[m]) - result_s13
-            soft_t = soft_thresholding(rx, self.mu)
-            # s15
-            Bx = np.multiply(np.divide(1.0, self.d_ATA[m]), soft_t)
-            # result_s21 = Bx_p - x_p
-            descent_D = Bx-x_block[m]
-
-            # time_s = time.time()
-            # result_s23 = gpu_cal.matmulvec(m, descent_D)
-            # result_s23 = gpu_cal.matMulVec_DiffSize(m, descent_D)
-            result_s23 = self.gpu_cal.matMulVec_DST(m, descent_D)
-            # time_mul += time.time() - time_s
-
-            # stepsize
-            r_1 = np.transpose(result_s11) @ result_s23 +\
-                self.mu*(np.linalg.norm(Bx, ord=1) -
-                         np.linalg.norm(x_block[m], ord=1))
-            r_2 = np.transpose(result_s23) @ result_s23
-            if r_2 == 0.0:
-                print('r_2 is ZERO, could not divide ZERO!')
-            else:
-                r = np.float64(element_proj(-r_1/r_2, 0, 1))
-
-            self.debug(result_s13, x_block[m], x, t, m, r)
-            self.err_record(err_iter, result_s13, x_block[m], t)
-
-            if IS_BOUNDED:
-                if not (self.DEBUG & self.ERR_RCD):
-                    self.error = error_crit(result_s13, x_block[m], self.mu)
-                if self.error < ERR_BOUND:
-                    block_Cnt += 1
-                if self.BLOCK - 1 == m:
-                    if block_Cnt == self.BLOCK:
-                        break
-                    else:
-                        block_Cnt = 0
-
-            # x(t+1) = x(t)+r(Bx(t)-x(t))
-            x_block[m] += r*(Bx-x_block[m])
-            x = np.vstack(x_block)
-            # Ax(t+1)
-            Ax[m] += r*result_s23
-
-            self.time_record(time_iter, t, start)
-            # print("matrix@vector:", time_mul,
-            #       "s, matrix.T@vector:", time_mul_t)
-
-        if self.TIME_RCD:
-            t_elapsed = time_iter[t]
-        else:
-            t_elapsed = time.time() - start
-
-        self.rlt_display(SILENCE, t_elapsed, t)
-        # PERFORMANCE = False
-        # if PERFORMANCE:
-        #     np.savetxt(settings.Dir_PERFORMANCE+"/GPU_time.txt", time_cnt)
-        #     np.savetxt(settings.Dir_PERFORMANCE+"/GPU_errors.txt", errors)
-
-        return t_elapsed
-
-
-class ClassLassoR(ClassLasso):
-    def __init__(self, gpu_cal, d_ATA, A, b, mu, BLOCK, ITER_MAX):
-        ClassLasso.__init__(self, gpu_cal, d_ATA, A,
-                            b, mu, BLOCK, ITER_MAX)
-        self.idx_shuffle = np.arange(self.BLOCK)
-
-    def index_get(self, t):
-        if t % self.BLOCK == 0:
-            random.shuffle(self.idx_shuffle)
-        return self.idx_shuffle[t % self.BLOCK]
-
-    def rlt_display(self, SILENCE, t_elapsed, t):
-        if not SILENCE:
-            print('GPU Random index, time used: {:.8f} s, '
-                  'with {:-4} loops, and block number: {:-2}.'.format(
-                      t_elapsed, t+1, self.BLOCK))
-
-
-class ClassLassoCPU(ClassLasso):
-    def __init__(self, A_block_p, d_ATA, A, b, mu, BLOCK, P, ITER_MAX):
-        ClassLasso.__init__(self, None, d_ATA, A, b, mu, BLOCK, ITER_MAX)
-        self.A_block_p = A_block_p
-        self.P = P
 
     def rlt_display(self, SILENCE, t_elapsed, t):
         if not SILENCE:
@@ -292,9 +158,136 @@ class ClassLassoCPU(ClassLasso):
             t_elapsed = time.time() - start
 
         self.rlt_display(SILENCE, t_elapsed, t)
-        # PERFORMANCE = False
-        # if PERFORMANCE:
-        #     np.savetxt(settings.Dir_PERFORMANCE+"/GPU_time.txt", time_cnt)
-        #     np.savetxt(settings.Dir_PERFORMANCE+"/GPU_errors.txt", errors)
 
         return t_elapsed
+
+
+class ClassLasso(ClassLassoCPU):
+    def __init__(self, gpu_cal, d_ATA, A, b, mu, BLOCK, ITER_MAX):
+        ClassLassoCPU.__init__(self, None, d_ATA, A, b, mu, BLOCK, None,
+                               ITER_MAX)
+        self.gpu_cal = gpu_cal
+
+    def rlt_display(self, SILENCE, t_elapsed, t):
+        if not SILENCE:
+            print('GPU Ascend index, time used: {:.8f} s, '
+                  'with {:-4} loops, and block number: {:-2}.'.format(
+                      t_elapsed, t+1, self.BLOCK))
+
+    def run(self, ERR_BOUND=None, err_iter=None, time_iter=None,
+            SILENCE=False, DEBUG=False):
+        # initialize
+        self.DEBUG = DEBUG
+        if isinstance(ERR_BOUND, float):
+            IS_BOUNDED = True
+        else:
+            IS_BOUNDED = False
+
+        if isinstance(err_iter, np.ndarray):
+            self.ERR_RCD = True
+        else:
+            self.ERR_RCD = False
+
+        if isinstance(time_iter, np.ndarray):
+            self.TIME_RCD = True
+        else:
+            self.TIME_RCD = False
+
+        x = np.zeros((self.A_SHAPE[1], 1))
+        x_block = np.asarray(np.vsplit(x, self.BLOCK))
+        Ax = np.zeros((self.BLOCK, self.A_SHAPE[0], 1))
+
+        block_Cnt = 0
+
+        # time_mul = 0
+        # time_mul_t = 0
+
+        start = time.time()
+        if self.TIME_RCD:
+            time_iter[0] = 0
+
+        for t in range(self.ITER_MAX):
+            # select mth block
+            m = self.index_get(t)
+            b_k = fun_b_k(Ax, self.b, m)
+            result_s11 = Ax[m] - b_k
+
+            # result_s13 = gpu_cal.mat_tmulvec(m, result_s11)
+            # time_s = time.time()
+            result_s13 = self.gpu_cal.mat_tMulVec_DiffSize(m, result_s11)
+            # time_mul_t += time.time() - time_s
+
+            # s14
+            rx = np.multiply(self.d_ATA[m], x_block[m]) - result_s13
+            soft_t = soft_thresholding(rx, self.mu)
+            # s15
+            Bx = np.multiply(np.divide(1.0, self.d_ATA[m]), soft_t)
+            # result_s21 = Bx_p - x_p
+            descent_D = Bx-x_block[m]
+
+            # time_s = time.time()
+            # result_s23 = gpu_cal.matmulvec(m, descent_D)
+            # result_s23 = gpu_cal.matMulVec_DiffSize(m, descent_D)
+            result_s23 = self.gpu_cal.matMulVec_DST(m, descent_D)
+            # time_mul += time.time() - time_s
+
+            # stepsize
+            r_1 = np.transpose(result_s11) @ result_s23 +\
+                self.mu*(np.linalg.norm(Bx, ord=1) -
+                         np.linalg.norm(x_block[m], ord=1))
+            r_2 = np.transpose(result_s23) @ result_s23
+            if r_2 == 0.0:
+                print('r_2 is ZERO, could not divide ZERO!')
+            else:
+                r = np.float64(element_proj(-r_1/r_2, 0, 1))
+
+            self.debug(result_s13, x_block[m], x, t, m, r)
+            self.err_record(err_iter, result_s13, x_block[m], t)
+
+            if IS_BOUNDED:
+                if not (self.DEBUG & self.ERR_RCD):
+                    self.error = error_crit(result_s13, x_block[m], self.mu)
+                if self.error < ERR_BOUND:
+                    block_Cnt += 1
+                if self.BLOCK - 1 == m:
+                    if block_Cnt == self.BLOCK:
+                        break
+                    else:
+                        block_Cnt = 0
+
+            # x(t+1) = x(t)+r(Bx(t)-x(t))
+            x_block[m] += r*(Bx-x_block[m])
+            x = np.vstack(x_block)
+            # Ax(t+1)
+            Ax[m] += r*result_s23
+
+            self.time_record(time_iter, t, start)
+            # print("matrix@vector:", time_mul,
+            #       "s, matrix.T@vector:", time_mul_t)
+
+        if self.TIME_RCD:
+            t_elapsed = time_iter[t]
+        else:
+            t_elapsed = time.time() - start
+
+        self.rlt_display(SILENCE, t_elapsed, t)
+
+        return t_elapsed
+
+
+class ClassLassoR(ClassLasso):
+    def __init__(self, gpu_cal, d_ATA, A, b, mu, BLOCK, ITER_MAX):
+        ClassLasso.__init__(self, gpu_cal, d_ATA, A,
+                            b, mu, BLOCK, ITER_MAX)
+        self.idx_shuffle = np.arange(self.BLOCK)
+
+    def index_get(self, t):
+        if t % self.BLOCK == 0:
+            random.shuffle(self.idx_shuffle)
+        return self.idx_shuffle[t % self.BLOCK]
+
+    def rlt_display(self, SILENCE, t_elapsed, t):
+        if not SILENCE:
+            print('GPU Random index, time used: {:.8f} s, '
+                  'with {:-4} loops, and block number: {:-2}.'.format(
+                      t_elapsed, t+1, self.BLOCK))
