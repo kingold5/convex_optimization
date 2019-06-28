601df4e17483e32cf1a05f94bd415583a8a1441f
kingold5 <xingran_wang@icloud.com>
2019-06-28 16:50:33 +0200

    add ClassLassoCB_v1 and ClassLassoCB_v2

--- cpu_vs_gpu.py
+++ cpu_vs_gpu.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# comparison between different CUDA algorithms and cpu algorithms
 
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from skcuda import cublas
+import pycuda.driver as cuda
 from gpu_calculation import GPU_Calculation
 from cpu_calculation import A_bp_get, fun_diag_ATA
 from parameters import parameters
-from lasso import ClassLasso, ClassLassoR, ClassLassoCPU
+from lasso import ClassLasso, ClassLassoR, ClassLassoCPU,\
+        ClassLassoCB_v1, ClassLassoCB_v2
 from average import list_aver
 import settings
 
@@ -31,36 +36,70 @@ def plt_fig(err_lasso, time_lasso, err_lasso_r, time_lasso_r):
     plt.show()
 
 
+def rlt_display(N, K, BLOCK, T_WIDTH, lasso_obj, time):
+    if (type(lasso_obj).__name__ == 'ClassLasso' or
+            type(lasso_obj).__name__ == 'ClassLassoR'):
+        print('matrix size ({:-5}, {:-5}), block: {}'
+              ', T_WIDTH=T_WIDTH_TRANS: {:-3}'
+              ', {:>18} time used: {:.8f} s'
+              ', standard deviation: {:.8f}'.format(
+                  N, K, BLOCK, T_WIDTH,
+                  lasso_obj.descript, np.mean(time), np.std(time)))
+    else:
+        print('matrix size ({:-5}, {:-5}), block: {},'
+              '                          '
+              ' {:>20} time used: {:.8f} s'
+              ', standard deviation: {:.8f}'.format(
+                  N, K, BLOCK, lasso_obj.descript,
+                  np.mean(time), np.std(time)))
+
+
 # density of sparse vector
 DENSITY = 0.4
 # error bound
 ERR_BOUND = 1e-4
-# generating matrix and not save
+# generating or load parameters
 READ_FLAG = False
+# save parameters or not
 SAVE_FLAG = False
-INSTANCE = 10
+INSTANCE = 1
 ITER_MAX = 1000
+WARM_UP = 4
+# row from 2 ** ROW_0 to 2 ** ROW_1
+ROW_0 = 10
+ROW_1 = 11
+# column from 2 ** (ROW+COLP_0) to 2 ** (ROW+COLP_1)
+COLP_0 = 2
+COLP_1 = 3
 P = 4
 
-# initial time and error recording array
+# time and error recording array
 t_lasso = np.zeros((INSTANCE, ITER_MAX+1))
-e_lasso = np.zeros((INSTANCE, ITER_MAX))
 t_lasso_r = np.zeros_like(t_lasso)
+t_lasso_cpu = np.zeros_like(t_lasso)
+t_lasso_cb_v1 = np.zeros_like(t_lasso)
+t_lasso_cb_v2 = np.zeros_like(t_lasso)
+e_lasso = np.zeros((INSTANCE, ITER_MAX))
 e_lasso_r = np.zeros_like(e_lasso)
+e_lasso_cpu = np.zeros_like(e_lasso)
+e_lasso_cb = np.zeros_like(e_lasso)
 t_comp = np.zeros((INSTANCE))
 t_comp_r = np.zeros_like(t_comp)
 t_comp_cpu = np.zeros_like(t_comp)
+t_comp_cb_v1 = np.zeros_like(t_comp)
+t_comp_cb_v2 = np.zeros_like(t_comp)
 
-for n_exp in np.arange(9, 10):
+# initial cublas context
+h = cublas.cublasCreate()
+
+for n_exp in np.arange(ROW_0, ROW_1):
     N = 2 ** n_exp
-    # was 1-16(0, 4)
-    for k_plus in np.arange(3, 4):
+    for k_plus in np.arange(COLP_0, COLP_1):
         k_exp = n_exp + k_plus
         K = 2 ** k_exp
-        # was np.arange(1+k_plus)
         for b_exp in np.arange(1, 2):
             BLOCK = 2 ** b_exp
-            for t_width in np.arange(4, 5):
+            for t_width in np.arange(8, 9):
                 # set thread width
                 GPU_Calculation.T_WIDTH_TRANS = 2 ** t_width
                 GPU_Calculation.T_WIDTH = 2 ** t_width
@@ -69,83 +108,95 @@ for n_exp in np.arange(9, 10):
                 t_comp.fill(0)
                 t_comp_r.fill(0)
                 t_comp_cpu.fill(0)
+                t_comp_cb_v1.fill(0)
+                t_comp_cb_v2.fill(0)
+
                 # reset time & error recording array
                 t_lasso.fill(0)
-                e_lasso.fill(0)
                 t_lasso_r.fill(0)
+                t_lasso_cpu.fill(0)
+                e_lasso.fill(0)
                 e_lasso_r.fill(0)
+                e_lasso_cpu.fill(0)
                 time_winit = 0
 
                 for i in range(INSTANCE):
-                    # make sure every random seed is unique
+                    # every random seed is unique
                     time.sleep(1)
                     (A, x_true, b, mu) = parameters(
                         N, K, DENSITY,
-                        SAVE_FLAG, READ_FLAG, SILENCE=True)
+                        SAVE_FLAG, READ_FLAG, SILENCE=False)
 
                     t_init = time.time()
                     gpu_cal = GPU_Calculation(A, BLOCK)
-                    A_block_p = A_bp_get(A, BLOCK, P)
+                    # A_block_p = A_bp_get(A, BLOCK, P)
                     d_ATA = gpu_cal.diag_ATA
-                    d_ATA_c = fun_diag_ATA(A_block_p)
-                    print(d_ATA - d_ATA_c)
-                    lasso = ClassLasso(gpu_cal, d_ATA, A, b, mu,
-                                       BLOCK, ITER_MAX)
-                    lasso_r = ClassLassoR(gpu_cal, d_ATA, A, b,
-                                          mu, BLOCK, ITER_MAX)
-                    lasso_cpu = ClassLassoCPU(A_block_p, d_ATA_c, A,
-                                              b, mu, BLOCK, P, ITER_MAX)
+                    # d_ATA_c = fun_diag_ATA(A_block_p)
+
+                    # lasso = ClassLasso(gpu_cal, d_ATA, A, b, mu,
+                    #                    BLOCK, ITER_MAX)
+                    # lasso_r = ClassLassoR(gpu_cal, d_ATA, A, b,
+                    #                       mu, BLOCK, ITER_MAX)
+                    # lasso_cpu = ClassLassoCPU(A_block_p, d_ATA, A,
+                    #                           b, mu, BLOCK, P, ITER_MAX)
+                    lasso_cb_v1 = ClassLassoCB_v1(h, gpu_cal, d_ATA, A,
+                                                  b, mu, BLOCK, ITER_MAX)
+                    lasso_cb_v2 = ClassLassoCB_v2(h, gpu_cal, d_ATA, A,
+                                                  b, mu, BLOCK, ITER_MAX)
                     time_winit += time.time() - t_init
 
-                    # let gpu warmup the first round
-                    # lasso(gpu_cal, d_ATA, A, b, mu, BLOCK,
-                    #       ITER_MAX, ERR_BOUND, SILENCE=True)
-                    t_comp[i] = lasso.run(ERR_BOUND,
-                                          SILENCE=False,
-                                          DEBUG=False)
-                    t_comp_r[i] = lasso_r.run(ERR_BOUND,
-                                              SILENCE=False)
-                    t_comp_cpu[i] = lasso_cpu.run(ERR_BOUND,
-                                                  SILENCE=False,
-                                                  DEBUG=False)
-                    print('')
-                    # time_comp.append(
-                    #     lasso(gpu_cal, d_ATA, A, b, mu, BLOCK,
-                    #           ITER_MAX, ERR_BOUND,
-                    #           err_iter=e_lasso[ii], time_iter=t_lasso[ii],
-                    #           SILENCE=True, DEBUG=False))
+                    # '''
+                    # let gpu warmup
+                    for _ in range(WARM_UP):
+                        # lasso.run(SILENCE=True,
+                        #           DEBUG=False)
+                        # lasso_r.run(ERR_BOUND,
+                        #             DEBUG=False,
+                        #             SILENCE=True)
+                        # lasso_cpu.run(
+                        #     ERR_BOUND,
+                        #     SILENCE=True,
+                        #     DEBUG=False)
 
-                    # time_comp_r.append(
-                    #     lasso_rand(gpu_cal, d_ATA, A, b, mu, BLOCK,
-                    #                ITER_MAX, ERR_BOUND,
-                    #                err_iter=e_lasso_r[ii],
-                    #                time_iter=t_lasso_r[ii],
-                    #                SILENCE=True, DEBUG=False))
+                        lasso_cb_v1.run(SILENCE=True,
+                                        DEBUG=False)
+                        lasso_cb_v2.run(SILENCE=True,
+                                        DEBUG=False)
+                    # '''
 
-                print('matrix size ({:-5}, {:-5})'.format(N, K)
-                      + ', block: ' + str(BLOCK)
-                      + ', T_WIDTH=T_WIDTH_TRANS: '
-                      + str(GPU_Calculation.T_WIDTH)
-                      + ', Ascend index GPU computation time: '
-                      + f'{np.mean(t_comp):.6f}' + ' s'
-                      + ', standard deviation: '
-                      + f'{np.std(t_comp):.6f}')
+                    # run instances
+                    # t_comp[i] = lasso.run(
+                    #     SILENCE=False,
+                    #     DEBUG=False)
+                    # t_comp_r[i] = lasso_r.run(ERR_BOUND,
+                    #                           SILENCE=False)
+                    # t_comp_cpu[i] = lasso_cpu.run(
+                    #     ERR_BOUND,
+                    #     SILENCE=False,
+                    #     DEBUG=False)
 
-                print('matrix size ({:-5}, {:-5})'.format(N, K)
-                      + ', block: ' + str(BLOCK)
-                      + ', T_WIDTH=T_WIDTH_TRANS: '
-                      + str(GPU_Calculation.T_WIDTH)
-                      + ', Random index GPU computation time: '
-                      + f'{np.mean(t_comp_r):.6f}' + ' s'
-                      + ', standard deviation: '
-                      + f'{np.std(t_comp_r):.6f}')
+                    t_comp_cb_v1[i] = lasso_cb_v1.run(
+                        SILENCE=False,
+                        DEBUG=False)
+                    t_comp_cb_v2[i] = lasso_cb_v2.run(
+                        SILENCE=False,
+                        DEBUG=False)
 
-                print('matrix size ({:-5}, {:-5})'.format(N, K)
-                      + ', block: ' + str(BLOCK)
-                      + ', Processor(s) used: ' + str(P)
-                      + ', Ascend index CPU computation time:'
-                      + f'{np.mean(t_comp_cpu):.6f}' + ' s'
-                      + ', standard deviation: '
-                      + f'{np.std(t_comp_cpu):.6f}')
+                # display results
+                # rlt_display(N, K, BLOCK, GPU_Calculation.T_WIDTH,
+                #             lasso, t_comp)
+                # rlt_display(N, K, BLOCK, GPU_Calculation.T_WIDTH,
+                #             lasso_r, t_comp_r)
+                # rlt_display(N, K, BLOCK, GPU_Calculation.T_WIDTH,
+                #             lasso_cpu, t_comp_cpu)
+                rlt_display(N, K, BLOCK, GPU_Calculation.T_WIDTH,
+                            lasso_cb_v1, t_comp_cb_v1)
+                rlt_display(N, K, BLOCK, GPU_Calculation.T_WIDTH,
+                            lasso_cb_v2, t_comp_cb_v2)
+                print('')
+
                 # print(time_winit / INSTANCE, 's.')
                 # plt_fig(err_lasso, time_lasso, err_lasso_r, time_lasso_r)
+
+cuda.stop_profiler()
+cublas.cublasDestroy(h)
