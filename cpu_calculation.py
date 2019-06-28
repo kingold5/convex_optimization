ba2cc3506c66cf173e6fad9bae63275260e6644f
kingold5 <xingran_wang@icloud.com>
2019-06-28 16:49:09 +0200

    delete fun_b_k function

--- cpu_calculation.py
+++ cpu_calculation.py
@@ -22,14 +22,9 @@ def error_crit(grad_fx, x, mu):
 
 def A_bp_get(A, BLOCK, P):
     A_SHAPE = A.shape
-    p_len = A_SHAPE[1]//(BLOCK * P)
+    HEIGHT = A_SHAPE[0]
 
-    A_block_p = np.empty((BLOCK, P, A_SHAPE[0], p_len))
-    for i in range(BLOCK):
-        for j in range(P):
-            p_idx = i * P + j
-            A_block_p[i][j] = A[:, p_idx*p_len: (p_idx+1)*p_len]
-    return A_block_p
+    return A.transpose().reshape(BLOCK, P, -1, HEIGHT).swapaxes(-1, -2)
 
 
 def fun_s12(A_bp, s11):
@@ -39,29 +34,16 @@ def fun_s12(A_bp, s11):
 # calculate blockwise diagonal of matrix A.transpose()*A
 def fun_diag_ATA(A_bp):
     BLOCK = A_bp.shape[0]
-    P = A_bp.shape[1]
     HEIGHT = A_bp.shape[2]
 
-    diag_ATA = np.empty((BLOCK, HEIGHT, P*A_bp.shape[3]))
-    for i in range(BLOCK):
-        diag_ATA[i] = np.hstack(A_bp[i])
+    diag_ATA = A_bp.swapaxes(-1, -2).reshape(
+        BLOCK, -1, HEIGHT).swapaxes(-1, -2)
+
     return np.sum(np.square(diag_ATA), axis=1)[:, :, np.newaxis]
 
 
 def fun_s22(A_bp, s21):
     return A_bp@s21
-
-
-# calculate b_k, excluded K_th block
-def fun_b_k(Ax, b, k):
-    if len(Ax) <= 1:
-        return b
-    else:
-        result = np.zeros((len(Ax[0]), 1))
-        for i in range(len(Ax)):
-            if i != k:
-                result += Ax[i]
-        return -result + b
 
 
 def fun_dd_p(P, descent_d):
