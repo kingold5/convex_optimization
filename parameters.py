159e07a065a8451f846605ce67bf501bcc930773
kingold5 <xingran_wang@icloud.com>
2019-07-16 20:02:49 +0200

    add complex parameter generation

--- parameters.py
+++ parameters.py
@@ -12,30 +12,29 @@ import settings
 
 def parameters(N, K, den, SAVE_FLAG, READ_FLAG, SILENCE=False):
     if not READ_FLAG:
-        # create matrix A, true vector x_true, and vector b
+        # create real number matrix A, original vector x_true, and vector b
         # with row-vector normalized matrix A (N by K)
         np.random.seed(int(time()))
-        A = []
-        # while np.linalg.matrix_rank(A) < np.minimum(N, K):
         A = np.random.randn(N, K)
         A = A/(np.linalg.norm(A, ord=2, axis=1, keepdims=True))
         # true sparse vector x_true with element value [0, 1]
         # dimension (K,1)
-        x_true = sparse.random(K, 1, density=den, format="csc",
-                               data_rvs=np.random.randn)
-        # x_true = np.zeros((K, 1))
-        # x_true[0: 50, 0] = 1
+        # x_true = sparse.random(K, 1, density=den, format="csr",
+        #                        data_rvs=np.random.randn)
+        x_true = np.zeros((K, 1))
+        x_true[0: 50, 0] = 1
         # vector b with dimension (N,1), b=A*x_true+e
         # with e i.i.d. gaussian distribution mean 0, variance 1e-4
         mean = 0
         SD = np.sqrt(1e-4)
         e = np.random.normal(mean, SD, (N, 1))
-        b = A@x_true + e
+        # convert scipy sparse array to numpy array for calculation in numpy
+        b = A@x_true.toarray() + e
         # regularization value mu, mu=0.1*|A^T*b|_inf
-        mu = 0.1*np.max(np.abs(np.transpose(A) @ b))
+        mu = 0.1*np.max(np.abs(A.T @ b))
 
         if not SILENCE:
-            print("Parameters @@created with N: %d" % N,
+            print("Real parameters @@created with N: %d" % N,
                   ", K: %d" % K,
                   ", DENSITY: %f" % den,
                   ", mu: %f." % mu)
@@ -52,7 +51,7 @@ def parameters(N, K, den, SAVE_FLAG, READ_FLAG, SILENCE=False):
             settings.HOME + "/Documents/python/parameters.txt")
 
         if not SILENCE:
-            print("Parameters @@loaded with N: %d" % N,
+            print("Real parameters @@loaded with N: %d" % N,
                   ", K: %d" % K,
                   ", DENSITY: %f" % DENSITY,
                   ", mu: %f" % mu, ".")
@@ -67,5 +66,68 @@ def parameters(N, K, den, SAVE_FLAG, READ_FLAG, SILENCE=False):
                    [N, K, den, mu])
 
         if not SILENCE:
-            print("Paramenters @@saved!")
+            print("Real paramenters @@saved!")
     return(A, x_true, b, mu)
+
+
+def parameters_comp(N, K, den, SAVE_FLAG, READ_FLAG, SILENCE=False):
+    if not READ_FLAG:
+        # create complex number matrix A, original vector x_true, and vector b
+        # with row-vector normalized matrix A (N by K)
+        np.random.seed(int(time()))
+        A = np.random.normal(loc=0, scale=np.sqrt(1/2), size=(N, K, 2))
+        A_comp = A.view(dtype=np.complex128)[..., 0]
+        # row normalize
+        A_comp = A_comp / np.linalg.norm(A_comp, ord=2, axis=1, keepdims=True)
+        # method to create stadard normal complex number
+        rvs = lambda s: np.random.normal(loc=0, scale=np.sqrt(1/2), size=s) +\
+            1j*np.random.normal(loc=0, scale=np.sqrt(1/2), size=s)
+        x_c = sparse.random(K, 1, density=den, format='csr',
+                                 dtype=np.complex128, data_rvs=rvs)
+        x_true_c = x_c.toarray()    
+        mean = 0
+        SD = np.sqrt(1e-4/2)
+        # add complex error to array b
+        e_c = np.random.normal(mean, SD, (N, 1)) +\
+            1j*np.random.normal(mean, SD, (N, 1))
+        b_c = A_comp @ x_true_c + e_c
+        # regularization value mu, mu=0.1*|A^T*b|_inf
+        mu = 0.1*np.max(np.abs(A_comp.conj().T @ b_c))
+
+        if not SILENCE:
+            print("Complex parameters @@created with N: %d" % N,
+                  ", K: %d" % K,
+                  ", DENSITY: %f" % den,
+                  ", mu: %f." % mu)
+    else:
+        # read the parameters from file
+        A_comp = np.loadtxt(settings.HOME+"/Documents/python/A_matrix_comp.txt",
+                            delimiter=",").view(np.complex128)
+        x_true_c = np.loadtxt(
+            settings.HOME+"/Documents/python/x_true_comp.txt").view(np.complex128)
+        x_true_c = x_true_c[:, np.newaxis]
+        b_c = np.loadtxt(
+            settings.HOME+"/Documents/python/b_vector_comp.txt").view(np.complex128)
+        b_c = b_c[:, np.newaxis]
+        N, K, DENSITY, mu = np.loadtxt(
+            settings.HOME + "/Documents/python/parameters_comp.txt")
+
+        if not SILENCE:
+            print("Complex parameters @@loaded with N: %d" % N,
+                  ", K: %d" % K,
+                  ", DENSITY: %f" % DENSITY,
+                  ", mu: %f" % mu, ".")
+
+    if SAVE_FLAG:
+        np.savetxt(settings.HOME+"/Documents/python/A_matrix_comp.txt",
+                   A_comp.view(np.float64), delimiter=",")
+        np.savetxt(settings.HOME+"/Documents/python/x_true_comp.txt",
+                   x_true_c.view(np.float64))
+        np.savetxt(settings.HOME+"/Documents/python/b_vector_comp.txt",
+                   b_c.view(np.float64))
+        np.savetxt(settings.HOME+"/Documents/python/parameters_comp.txt",
+                   [N, K, den, mu])
+
+        if not SILENCE:
+            print("Complex paramenters @@saved!")
+    return(A_comp, x_true_c, b_c, mu)
