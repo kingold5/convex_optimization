bbe8e2e92ba8cabf07aaf9482e8b23e00f193ee7
kingold5 <xingran_wang@icloud.com>
2019-07-21 19:18:08 +0200

    delete comment_proj error_crit fun_s12 fun_s22 fun_b_k

--- cpu_calculation.py
+++ cpu_calculation.py
@@ -2,33 +2,11 @@
 import numpy as np
 
 
-def soft_thresholding(tensor, threshold):
-    return np.sign(tensor)*np.maximum(np.abs(tensor)-threshold, 0)
-
-
-# project vec onto [lower_bound, upper_bound]
-def element_proj(vec, lower_bound, upper_bound):
-    return np.maximum(np.minimum(vec, upper_bound), lower_bound)
-
-
-# calculate error
-def error_crit(grad_fx, x, mu):
-    projection = element_proj(grad_fx-x, -mu, mu)
-    # l2 norm
-    # return np.linalg.norm(grad_fx-projection, ord=2)
-    # l infinity norm
-    return np.max(np.abs(grad_fx-projection))
-
-
 def A_bp_get(A, BLOCK, P):
     A_SHAPE = A.shape
     HEIGHT = A_SHAPE[0]
 
     return A.transpose().reshape(BLOCK, P, -1, HEIGHT).swapaxes(-1, -2)
-
-
-def fun_s12(A_bp, s11):
-    return A_bp.T@s11
 
 
 # calculate blockwise diagonal of matrix A.transpose()*A
@@ -51,7 +29,3 @@ def fun_diag_ATA_c(A_bp):
         BLOCK, -1, HEIGHT).swapaxes(-1, -2)
 
     return np.sum(diag_ATA.conj() * diag_ATA, axis=1)[..., np.newaxis]
-
-
-def fun_s22(A_bp, s21):
-    return A_bp@s21
